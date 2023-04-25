7aca5316c12894337e3f77c6cfbd709cdf017d08
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2023-04-25 19:55:07 +0100

    Use standard stl container for boards

--- PinkySource/Gomoku.cpp
+++ PinkySource/Gomoku.cpp
@@ -6,13 +6,13 @@ const Gomoku::t_patterns Gomoku::_attack_patterns = {
             { 0b0101010101, INTMAX_MAX },
             { 0b0001010101, 4 },
             { 0b0101010100, 4 },
-            { 0b0000010101, 3 },
-            { 0b0001010100, 3 },
-            { 0b0101010000, 3 },
-            { 0b0000000101, 2 },
-            { 0b0000010100, 2 },
-            { 0b0001010000, 2 },
-            { 0b0101000000, 2 },
+            { 0b0000010101, 300 },
+            { 0b0001010100, 300 },
+            { 0b0101010000, 300 },
+            { 0b0000000101, 100 },
+            { 0b0000010100, 100 },
+            { 0b0001010000, 100 },
+            { 0b0101000000, 100 },
             { 0b0000000001, 1 },
             { 0b0000000100, 1 },
             { 0b0000010000, 1 },
@@ -26,13 +26,13 @@ const Gomoku::t_patterns Gomoku::_attack_patterns = {
             { 0b1010101010, INTMAX_MAX },
             { 0b0010101010, 4 },
             { 0b1010101000, 4 },
-            { 0b0000101010, 3 },
-            { 0b0010101000, 3 },
-            { 0b1010100000, 3 },
-            { 0b0000001010, 2 },
-            { 0b0000101000, 2 },
-            { 0b0010100000, 2 },
-            { 0b1010000000, 2 },
+            { 0b0000101010, 300 },
+            { 0b0010101000, 300 },
+            { 0b1010100000, 300 },
+            { 0b0000001010, 200 },
+            { 0b0000101000, 200 },
+            { 0b0010100000, 200 },
+            { 0b1010000000, 200 },
             { 0b0000000010, 1 },
             { 0b0000001000, 1 },
             { 0b0000100000, 1 },
@@ -84,134 +84,116 @@ const std::vector<Gomoku::t_coord> Gomoku::_directions = {
     {1, 1}
 };
 
-Gomoku::t_board *Gomoku::new_board(size_t board_size)
+
+uint64_t *Gomoku::copy_board(uint64_t *board)
 {
-    t_board *board;
-    size_t  bytes;
+    uint64_t *board_copy;
 
-    board = new t_board;
-    board->board_buffer = new uint64_t[board_size];
-    board->board_size = board_size;
-    board->next = NULL;
-    board->prev = NULL;
-    return (board);
-}
-
-Gomoku::t_board *Gomoku::copy_board(t_board *board)
-{
-    t_board *board_copy;
-
-    board_copy = new_board(board->board_size);
-    memcpy(board_copy->board_buffer, board->board_buffer,
-            board->board_size * sizeof(uint64_t));
+    board_copy = new uint64_t[this->_board_size];
+    for (short i = 0; i < this->_board_size; i++)
+        board_copy[i] = board[i];
     return (board_copy);
 }
 
-Gomoku::t_board *Gomoku::update_board(t_board *board, t_coord piece_coord, t_piece piece)
+uint64_t *Gomoku::update_board(uint64_t *board, t_coord piece_coord, t_piece piece)
 {
-    t_board *new_board;
+    uint64_t *new_board;
 
     new_board = copy_board(board);
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    new_board->board_buffer[piece_coord.y] |= uint64_t(piece) << (piece_coord.x * 2);
+    new_board[piece_coord.y] |= uint64_t(piece) << (piece_coord.x * 2);
 #else
-    new_board->board_buffer[piece_coord.y] |= uint64_t(piece) >> (piece_coord.x * 2);
+    new_board[piece_coord.y] |= uint64_t(piece) >> (piece_coord.x * 2);
 # endif
     return (new_board);
-}
-void Gomoku::push_move(t_board *board)
-{
-    if (this->_move_history != NULL)
-    {
-        this->_move_history->next = board;
-        board->prev = this->_move_history;
-    }
-    this->_move_history = board;
 }
 
 void Gomoku::print_board()
 {
-    t_board *board = this->_move_history;
+    uint64_t *board = this->_move_history.front();
 
-    for (short y = 0; y < board->board_size; y++)
+    for (short y = 0; y < this->_board_size; y++)
     {
-        for (short x = 0; x < board->board_size; x++)
+        for (short x = 0; x < this->_board_size; x++)
         {
             t_piece piece = this->get_piece(board, (t_coord){x, y});
-            if (piece == Gomoku::BLACK)
-                std::cout << "X ";
-            else if (piece == Gomoku::WHITE)
-                std::cout << "O ";
-            else
-                std::cout << ". ";
+            switch (piece)
+            {
+                case Gomoku::BLACK:
+                    std::cout << "X ";
+                    break;
+                case Gomoku::WHITE:
+                    std::cout << "O ";
+                    break;
+                case Gomoku::EMPTY:
+                    std::cout << ". ";
+                    break;
+            }
         }
         std::cout << std::endl;
     }
 }
 
 Gomoku::Gomoku(uint8_t board_size, t_piece player_color, t_difficulty difficulty):
-    _move_history(NULL), _player_color(player_color), _difficulty(difficulty), _turn(0)
+    _player_color(player_color), _difficulty(difficulty), _turn(0), _board_size(board_size)
 {
 
     if (board_size < 5 || board_size > 19)
         throw std::invalid_argument("Board size must be between 5 and 19");
-   this->push_move(this->new_board(board_size));
+   this->_move_history.push_front(new uint64_t[board_size]);
    this->_ai_color = (this->_player_color == Gomoku::BLACK) ? Gomoku::WHITE : Gomoku::BLACK;
 }
 
 Gomoku::~Gomoku()
 {
-    for (t_board *board = this->_move_history; board != NULL;)
-    {
-        t_board *tmp = board->prev;
-        delete[] board->board_buffer;
-        delete board;
-        board = tmp;
-    }
+    for (auto board : this->_move_history)
+        delete[] board;
 }
 
-Gomoku::t_piece Gomoku::get_piece(t_board *board, t_coord piece_coord)
+Gomoku::t_piece Gomoku::get_piece(uint64_t *board, t_coord piece_coord)
 {
     uint64_t piece;
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    piece = (board->board_buffer[piece_coord.y] >> (piece_coord.x * 2)) & 0b11;
+    piece = (board[piece_coord.y] >> (piece_coord.x * 2)) & 0b11;
 #else
-    piece = (board->board_buffer[piece_coord.y] << (piece_coord.x * 2)) & 0b11;
+    piece = (board[piece_coord.y] << (piece_coord.x * 2)) & 0b11;
 #endif
     return (t_piece(piece));
 }
 
 void    Gomoku::register_move(t_coord piece_coord, t_piece piece)
 {
-    this->push_move(this->update_board(this->_move_history, piece_coord, piece));
+    this->_move_history.push_front(this->update_board(this->_move_history.front(), piece_coord, piece));
     this->_turn++;
 }
 
-uint64_t Gomoku::evaluate_dir(t_board *board, t_coord piece_coord, t_piece piece, t_coord direction)
+uint64_t Gomoku::evaluate_dir(uint64_t *board, t_coord piece_coord, t_piece piece, t_coord direction)
 {
-    uint64_t    score;
+    uint32_t    score;
     uint16_t    current_pattern;
     t_coord     current_position;
-    const std::map<uint16_t, uint16_t> &attack_patterns = Gomoku::_attack_patterns.at(piece);
-    const std::map<uint16_t, uint16_t> &defense_patterns = Gomoku::_defense_patterns.at(piece);
+    const std::map<uint16_t, uint32_t> &attack_patterns = Gomoku::_attack_patterns.at(piece);
+    const std::map<uint16_t, uint32_t> &defense_patterns = Gomoku::_defense_patterns.at(piece);
 
     score = 0;
     current_pattern = 0;
     current_position = piece_coord;
     do
     {
-        if (current_position.x < 0 || current_position.x >= board->board_size)
+        if (current_position.x < 0 || current_position.x >= this->_board_size)
             break;
         current_position.y = piece_coord.y;
         do
         {
-            if (current_position.y < 0 || current_position.y >= board->board_size)
+            if (current_position.y < 0 || current_position.y >= this->_board_size)
                 break;
             current_pattern <<= 2;
             current_pattern |= this->get_piece(board, current_position);
             if (attack_patterns.contains(current_pattern))
-                score += attack_patterns.at(current_pattern);
+                score = std::max(attack_patterns.at(current_pattern), score);
+            else
+                break;
             current_position.y += direction.y;
         } while(direction.y && std::abs(piece_coord.y - current_position.y) < 5);
         current_position.x += direction.x;
@@ -220,7 +202,7 @@ uint64_t Gomoku::evaluate_dir(t_board *board, t_coord piece_coord, t_piece piece
 }
 
 
-uint64_t Gomoku::evaluate_move(t_board *board, t_coord piece_coord, t_piece piece)
+uint64_t Gomoku::evaluate_move(uint64_t *board, t_coord piece_coord, t_piece piece)
 {
     uint64_t score = 0;
 
@@ -233,11 +215,11 @@ uint64_t Gomoku::evaluate_move(t_board *board, t_coord piece_coord, t_piece piec
 uint64_t Gomoku::test_evaluate_board(t_piece piece)
 {
     uint64_t score = 0;
-    t_board *board = this->_move_history;
+    uint64_t *board = this->_move_history.front();
     for (int i = 0;  i < 9320; i ++)
     {
-        for (short y = 0; y < board->board_size; y++)
-            for (short x = 0; x < board->board_size; x++)
+        for (short y = 0; y < this->_board_size; y++)
+            for (short x = 0; x < this->_board_size; x++)
                 score += this->evaluate_move(board, (t_coord){x, y}, piece);
     }
     return (score);
@@ -247,19 +229,35 @@ int main()
 {
     Gomoku game(19, Gomoku::BLACK, Gomoku::EASY);
     // game.register_move((Gomoku::t_coord){18, 18}, Gomoku::WHITE);
-    // std::cout << game.evaluate_move(game._move_history, (Gomoku::t_coord){18, 18},  Gomoku::WHITE) << std::endl;
+    // std::cout << game.evaluate_move(game._move_history.front(), (Gomoku::t_coord){18, 18},  Gomoku::WHITE) << std::endl;
     // game.print_board();
     // std::cout << "-----------------------" << std::endl;
     // game.register_move((Gomoku::t_coord){16, 18}, Gomoku::WHITE);
-    // std::cout << game.evaluate_move(game._move_history, (Gomoku::t_coord){16, 18},  Gomoku::WHITE) << std::endl;
+    // std::cout << game.evaluate_move(game._move_history.front(), (Gomoku::t_coord){16, 18},  Gomoku::WHITE) << std::endl;
     // game.print_board();
     // std::cout << "-----------------------" << std::endl;
     // game.register_move((Gomoku::t_coord){18, 17}, Gomoku::WHITE);
-    // std::cout << game.evaluate_move(game._move_history, (Gomoku::t_coord){18, 17},  Gomoku::WHITE) << std::endl;
+    // std::cout << game.evaluate_move(game._move_history.front(), (Gomoku::t_coord){18, 17},  Gomoku::WHITE) << std::endl;
     // game.print_board();
     // std::cout << "-----------------------" << std::endl;
     // game.register_move((Gomoku::t_coord){17, 18}, Gomoku::WHITE);
-    // std::cout << game.evaluate_move(game._move_history, (Gomoku::t_coord){17, 18},  Gomoku::WHITE) << std::endl;
+    // std::cout << game.evaluate_move(game._move_history.front(), (Gomoku::t_coord){17, 18},  Gomoku::WHITE) << std::endl;
+    // game.print_board();
+    // std::cout << "-----------------------" << std::endl;
+    // game.register_move((Gomoku::t_coord){10, 18}, Gomoku::WHITE);
+    // std::cout << game.evaluate_move(game._move_history.front(), (Gomoku::t_coord){18, 18},  Gomoku::WHITE) << std::endl;
+    // game.print_board();
+    // std::cout << "-----------------------" << std::endl;
+    // game.register_move((Gomoku::t_coord){12, 18}, Gomoku::WHITE);
+    // std::cout << game.evaluate_move(game._move_history.front(), (Gomoku::t_coord){16, 18},  Gomoku::WHITE) << std::endl;
+    // game.print_board();
+    // std::cout << "-----------------------" << std::endl;
+    // game.register_move((Gomoku::t_coord){14, 17}, Gomoku::WHITE);
+    // std::cout << game.evaluate_move(game._move_history.front(), (Gomoku::t_coord){18, 17},  Gomoku::WHITE) << std::endl;
+    // game.print_board();
+    // std::cout << "-----------------------" << std::endl;
+    // game.register_move((Gomoku::t_coord){15, 17}, Gomoku::WHITE);
+    // std::cout << game.evaluate_move(game._move_history.front(), (Gomoku::t_coord){17, 18},  Gomoku::WHITE) << std::endl;
     // game.print_board();
     // std::cout << "-----------------------" << std::endl;
     game.test_evaluate_board(Gomoku::WHITE);
--- PinkySource/Gomoku.hpp
+++ PinkySource/Gomoku.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <limits>
 #include <map>
+#include <list>
+#include <algorithm>
 /**
  * @brief Game class
  * This game is a simple implementation of the gomoku game using minimax algorithm.
@@ -38,44 +40,36 @@ class Gomoku
         }                   t_coord;
 
     private:
-        typedef struct      s_board
-        {
-            uint64_t        *board_buffer;
-            uint8_t         board_size;
-            struct s_board  *next;
-            struct s_board  *prev;
-        }                   t_board;
-
-    private:
-        typedef std::map< Gomoku::e_piece, std::map<uint16_t, uint16_t> > t_patterns;
+        typedef std::map< Gomoku::e_piece, std::map<uint16_t, uint32_t> > t_patterns;
         const static std::vector<t_coord>   _directions;
         const static t_patterns             _attack_patterns;
         const static t_patterns             _defense_patterns;
     
-    private:
-        t_board         *_move_history;
-        t_piece         _ai_color;
-        t_piece         _player_color;
-        t_difficulty    _difficulty;
-        size_t          _turn;
+    public:
+        uint8_t                 _board_size;
+        std::list<uint64_t*>    _move_history;
+        t_piece                 _ai_color;
+        t_piece                 _player_color;
+        t_difficulty            _difficulty;
+        size_t                  _turn;
 
     public:
-                Gomoku(uint8_t board_size, t_piece player_color, t_difficulty difficulty);
-                ~Gomoku();
-        void    register_move(t_coord piece_coord, t_piece piece);
+                    Gomoku(uint8_t board_size, t_piece player_color, t_difficulty difficulty);
+                    ~Gomoku();
+        void        register_move(t_coord piece_coord, t_piece piece);
         void        print_board();
         uint64_t    test_evaluate_board(t_piece piece);
+        uint64_t    evaluate_move(uint64_t *board, t_coord piece_coord, t_piece piece);
 
     private:
-        t_board     *update_board(t_board *board, t_coord piece_coord, t_piece piece);
-        t_board     *new_board(size_t board_size);
-        t_board     *copy_board(t_board *board);
-        t_piece     get_piece(t_board *board, t_coord piece_coord);
-        void        push_move(t_board *board);
-        t_board     *minimax(t_board *board, t_piece piece, size_t depth);
-        t_board     *maximize(t_board *board, t_piece piece, size_t depth, size_t aplha);
-        t_board     *minimize(t_board *board, t_piece piece, size_t depth, size_t beta);
-        uint64_t    evaluate_move(t_board *board, t_coord piece_coord, t_piece piece);
-        uint64_t    evaluate_dir(t_board *board, t_coord piece_coord, t_piece piece, t_coord direction);
+        t_piece     get_piece(uint64_t *board, t_coord piece_coord);
+        void        push_move(uint64_t *board);
+        uint64_t    *new_board(size_t board_size);
+        uint64_t    *copy_board(uint64_t *board);
+        uint64_t    *update_board(uint64_t *board, t_coord piece_coord, t_piece piece);
+        uint64_t    evaluate_dir(uint64_t *board, t_coord piece_coord, t_piece piece, t_coord direction);
+        uint64_t    *minimax(uint64_t *board, t_piece piece, size_t depth);
+        uint64_t    *maximize(uint64_t *board, t_piece piece, size_t depth, size_t aplha);
+        uint64_t    *minimize(uint64_t *board, t_piece piece, size_t depth, size_t beta);
 
 };
