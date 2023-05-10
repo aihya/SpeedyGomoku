3ad6c5c7aedc64da5d693ce87950ae9a4140cda5
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-05-10 17:00:10 +0100

    set quit attribute in Window class to be shared on all surfaces

--- ui/visualizer.py
+++ ui/visualizer.py
@@ -1,50 +1,15 @@
+import asyncio
 import pygame
 from surface    import Surface
 from fonts      import *
 from init       import *
-from random     import randint
-from computer   import Computer
 from setup      import Setup
 from board      import Board
 from final      import Final
-
-
-# Setup relevant variables
-QUIT = False
-HEIGHT = 800
-WIDTH  = 1400
-BOARD_COLOR = pygame.Color("#EAE6E3")
-WHITE = pygame.Color("#ffffff")
-BLACK = pygame.Color("#000000")
-
+from state      import State
 
 # Initialize pygame
 pygame.init()
-
-
-class State:
-    """
-    This class represents a board state read from the game logic.
-    """
-
-    __slots__ = ('_state',)
-
-    def __init__(self):
-        self._state = [[str(randint(0, 2)) for j in range(19)] for i in range(19)]
-
-    def __getitem__(self, index):
-        return self.state[index]
-
-    @property
-    def state(self):
-        return self._state
-
-    @state.setter
-    def state(self, new_state):
-        self._state = new_state
-
-    def update(self, x, y, player):
-        self[y][x] = player
 
 
 class Window:
@@ -52,12 +17,13 @@ class Window:
     This class represents the display surface.
     """
 
-    __slots__ = ('_width', '_height', '_surface')
+    __slots__ = ('_width', '_height', '_surface', '_quit')
 
     def __init__(self):
         self._width   = WIDTH
         self._height  = HEIGHT
         self._surface = pygame.display.set_mode((self._width, self._height))
+        self._quit    = False
 
     @property
     def width(self):
@@ -66,6 +32,14 @@ class Window:
     @property
     def height(self):
         return self._height
+
+    @property
+    def quit(self):
+        return self._quit
+    
+    @quit.setter
+    def quit(self, value):
+        self._quit = value
 
     @property
     def surface(self):
@@ -122,15 +96,14 @@ class Game:
         return self._state
 
     def run(self):
-        global QUIT
-
-        while not QUIT:
+        while self.window.quit == False:
             if self.current_surface == SETUP_SURFACE:
                 self.current_surface = self.setup_surface.loop()
             elif self.current_surface == BOARD_SURFACE:
                 self.current_surface = self.board_surface.loop()
             elif self.current_surface == FINAL_SURFACE:
                 self.current_surface = self.final_surface.loop()
+        return None
 
 
 if __name__ == "__main__":
