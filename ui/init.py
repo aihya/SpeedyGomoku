12e4127e10fddb03a15a4baa6b76c5130451e303
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-08-09 17:45:32 +0100

    extended init macros

--- ui/init.py
+++ ui/init.py
@@ -14,20 +14,34 @@ BOARD_SURFACE = 2
 FINAL_SURFACE = 3
 STATS_SURFACE = 4
 
-# Constants
+# Dimentions
 HEIGHT      = 800
 WIDTH       = 1400
-WHITE       = pygame.Color("#ffffff")
-BLACK       = pygame.Color("#000000")
+
+# Colors
+GRAY_1 = pygame.Color("#252525")
+GRAY_2 = pygame.Color("#323232")
+GRAY_3 = pygame.Color("#4B4B4B")
+LIGHT  = pygame.Color("#E0E0E0")
+ACCENT = pygame.Color("#E43F5A")
+WHITE  = pygame.Color("#ffffff")
+BLACK  = pygame.Color("#000000")
 BOARD_COLOR = pygame.Color("#F2A71B")
 DEFAULT_BG  = pygame.Color("#EAE6E3")
-WHITE_COLOR = pygame.Color("#ffffff")
-BLACK_COLOR = pygame.Color("#000000")
+
+# Checkbox alignment
+VERTICAL    = 1
+HORIZONTAL  = 2
 
 # Player type
 HUMAN       = 1
 COMPUTER    = 2
 TYPES       = ['human', 'ai']
+
+# Rules
+STANDARD    = 1
+PRO         = 2
+LONG_PRO    = 3
 
 # Computer difficulty
 EASY        = 1
